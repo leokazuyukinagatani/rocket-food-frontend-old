9636b4fb2f191dd268dcac68208f63e212756af0
Leo Kazuyuki Nagatani <lknlkn15@hotmail.com>
2022-10-19 11:54:34 -0300

    config: tailwind and storybook

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -4,6 +4,33 @@ module.exports = {
     './src/**/*.tsx',
   ],
   theme: {
+    colors: {
+      transparent: 'transparent',
+      white: '#FFFFFF',
+      black: '#000000',
+      gray: {
+        900: '#000a0f',
+        500: '#c3c3cb',
+        400: '#7c7c8a'
+      },
+      blue: {
+        900: '#00111a',
+        800: '#1a2328',
+        700: '#0d1e26',
+        600: '#00344d',
+        100: '#80f2ff'
+      },
+      yellow: {
+        500: '#fba94b'
+      },
+      green: {
+        500: '#04d261'
+      },
+      red: {
+        400: '#ac4d55',
+        500: '#94000f'
+      }
+    },
     extend: {},
   },
   plugins: [],
